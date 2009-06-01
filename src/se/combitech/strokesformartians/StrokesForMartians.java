9b4ecdfd8248d98984a75e9c614097c4a949e297
moushkka <moushkka@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-06-01 16:02:58 +0000

    first!!

---
+++ src/se/combitech/strokesformartians/StrokesForMartians.java
@@ -0,0 +1,13 @@
+package se.combitech.strokesformartians;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StrokesForMartians extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
