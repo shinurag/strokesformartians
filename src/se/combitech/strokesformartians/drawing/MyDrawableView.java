ea54fffe0d2a42cbce93f69397a132ddc356de2f
moushkka <moushkka@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-06-15 16:20:26 +0000

    Added package: Drawing for 2D

---
+++ src/se/combitech/strokesformartians/drawing/MyDrawableView.java
@@ -0,0 +1,13 @@
+package se.combitech.strokesformartians.drawing;
+
+import android.content.Context;
+import android.view.View;
+
+public class MyDrawableView extends View {
+
+	public MyDrawableView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
