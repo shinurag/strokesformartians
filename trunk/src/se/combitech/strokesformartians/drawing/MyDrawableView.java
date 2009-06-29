a0bd7b39bb155713a46a82451fae90407b163509
hongenmeister <hongenmeister@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-06-29 18:20:52 +0000

    Functional MyDrawableView No touchie though

--- trunk/src/se/combitech/strokesformartians/drawing/MyDrawableView.java
+++ trunk/src/se/combitech/strokesformartians/drawing/MyDrawableView.java
@@ -1,25 +1,48 @@
 package se.combitech.strokesformartians.drawing;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Bitmap.Config;
+import android.graphics.Paint.Style;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MyDrawableView extends View {
 	private ShapeDrawable shapeDrawable;
+	public Bitmap bitmap;
 	
 	public MyDrawableView(Context context) {
 		super(context);
 		
 		shapeDrawable = new ShapeDrawable(new RectShape());
-		shapeDrawable.getPaint().setColor(Color.CYAN);
-		shapeDrawable.setBounds(10, 10, 100, 100);
+		shapeDrawable.getPaint().setColor(Color.GRAY);
+		shapeDrawable.setBounds(1, 1, 200, 250);
+		
+		bitmap = Bitmap.createBitmap(200, 250, Config.ARGB_4444);
 	}
 	
 	protected void onDraw(Canvas canvas) {
         shapeDrawable.draw(canvas);
-    }
+       
+        int x = 0;
+        int y = 0;
 
+        Paint paint = new Paint();
+        
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Style.FILL);
+        //canvas.drawCircle(50, 100, 20, paint);
+        
+        canvas.drawBitmap(bitmap, 1, 1, paint);
+    }
+	
+	public boolean onTouchEvent(MotionEvent me) {
+		bitmap.setPixel((int)me.getX(), (int)me.getY(), Color.BLUE);
+		return super.onTouchEvent(me); 
+	}
 }
