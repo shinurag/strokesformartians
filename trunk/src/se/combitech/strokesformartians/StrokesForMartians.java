87b37f2e6d050f4a6866f95700e7cb7e406de85f
richard.thule <richard.thule@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-07-13 17:04:15 +0000

    Vi försöker skapa lite credits

--- trunk/src/se/combitech/strokesformartians/StrokesForMartians.java
+++ trunk/src/se/combitech/strokesformartians/StrokesForMartians.java
@@ -1,11 +1,13 @@
 package se.combitech.strokesformartians;
 
+import se.combitech.strokesformartians.credits.Credits;
 import se.combitech.strokesformartians.dancing.DanceAnimator;
 import se.combitech.strokesformartians.drawing.Paint;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,10 +21,12 @@ public class StrokesForMartians extends Activity {
         
     	final Intent paintIntent = new Intent( this, Paint.class );
         final Intent danceIntent = new Intent( this, DanceAnimator.class );
+        final Intent creditIntent = new Intent( this, Credits.class );
         
         LinearLayout layout = new LinearLayout(this);
         final Button paintButton = new Button(this);
         final Button danceButton = new Button(this);
+        final Button creditButton = new Button(this);
         paintButton.setText( "Paint!" );
         paintButton.setOnClickListener(new OnClickListener()
         {
@@ -56,9 +60,26 @@ public class StrokesForMartians extends Activity {
         	}
         });
 
+        creditButton.setText( "Credits" );
+        creditButton.setOnClickListener(new OnClickListener()
+        {
+        	public void onClick(View view)
+        	{
+        		try
+        		{
+        			startActivity( creditIntent );
+        		}
+        		catch( ActivityNotFoundException anfe )
+        		{
+        			System.out.println( anfe.getMessage() );
+        		}
+        	}
+        });
+        
         
         layout.addView( paintButton );
         layout.addView( danceButton );
+        layout.addView( creditButton );
         
         setContentView(layout);
     }
---
+++ trunk/src/se/combitech/strokesformartians/credits/Credits.java
@@ -0,0 +1,23 @@
+package se.combitech.strokesformartians.credits;
+
+import se.combitech.strokesformartians.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.FrameLayout.LayoutParams;
+
+public class Credits extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Lina\nHenrik");
+        setContentView(tv);
+
+	}
+}
