a925038c827e0fa16b8d4d4d0d2fd2cc18de2559
moushkka <moushkka@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-07 18:11:22 +0000

    I can count to potato.

--- src/se/combitech/strokesformartians/drawing/BrushSizeDialog.java
+++ src/se/combitech/strokesformartians/drawing/BrushSizeDialog.java
@@ -2,11 +2,16 @@ package se.combitech.strokesformartians.drawing;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.LinearLayout.LayoutParams;
 
 public class BrushSizeDialog extends Dialog{
 	final float MAX_SIZE = 132.0f;
@@ -44,22 +49,47 @@ public class BrushSizeDialog extends Dialog{
 			}
         };
 
-		SeekBar seekBar = new SeekBar(getContext());
-		//seekBar.setMax((int)MAX_SIZE);
-		seekBar.setMax(100);
-		seekBar.setProgress((int)MAX_SIZE);
-		seekBar.setMinimumWidth(100);
-		
+        
 		Button submitButton = new Button(getContext());
 		submitButton.setText("Select");
-		
+        
+        
+        //LinearLayout layout = new LinearLayout(getContext());
+        //layout.setLayoutParams(new
+        //LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT,
+        //LinearLayout.LayoutParams.WRAP_CONTENT));
+        //layout.setMinimumWidth(400);
+        //layout.setPadding(20, 20, 20, 20);
+        
 		LinearLayout myLayout = new LinearLayout(getContext());
+		myLayout.setMinimumWidth(200);        
+		myLayout.setPadding(20, 0, 20, 20);
+		
+//		myLayout.setGravity(Gravity.TOP);
+		myLayout.setGravity(Gravity.FILL_VERTICAL);
+		myLayout.setLayoutParams(new LinearLayout.LayoutParams(300,36));
+		myLayout.setOrientation(LinearLayout.VERTICAL);
+		
+		//myLayout.setLayoutParams(new 
+		//		LinearLayout.LayoutParams(0,LinearLayout.LayoutParams.WRAP_CONTENT));
+		
+		Button helloButton = new Button(getContext());
+		helloButton.setTag("Ok");
+		
+		SeekBar seekBar = new SeekBar(getContext());
+        seekBar.setMax(32);
+        seekBar.setLayoutParams(new
+        ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT));
+        seekBar.setProgress(0); //
+        seekBar.setProgress(5);
 		myLayout.addView(seekBar);
-		myLayout.setMinimumWidth(300);
+		myLayout.addView(helloButton);
+		myLayout.bringChildToFront(seekBar);
 		
-		
-		
-		setContentView(myLayout);        
+		//setContentView(myLayout);
+		setContentView(myLayout);
+		setTitle("Pick a brush size, fool!");
         //setContentView(new SizePickerView(getContext(), l, mInitialSize));
         //setTitle("Pick a Brush Size!");
     }
