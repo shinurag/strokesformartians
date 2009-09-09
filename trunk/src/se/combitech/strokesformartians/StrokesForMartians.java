3c91671ccc78cdcbd383135fb050573722fc5ef1
hongenmeister <hongenmeister@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-09 17:17:49 +0000

    Uses the Android menu button instead of buttons in the GUI

--- trunk/src/se/combitech/strokesformartians/StrokesForMartians.java
+++ trunk/src/se/combitech/strokesformartians/StrokesForMartians.java
@@ -1,91 +1,53 @@
 package se.combitech.strokesformartians;
 
-import se.combitech.strokesformartians.credits.Credits;
 import se.combitech.strokesformartians.dancing.Dancer;
-import se.combitech.strokesformartians.drawing.FingerPaint;
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.SeekBar;
+
 
 public class StrokesForMartians extends Activity {
+	
+	private Intent danceIntent = null;
+	
+    public static final int MENU_LOAD_ID = 0;
+    public static final int MENU_DRAW_ID = 1;
+    public static final int MENU_CREDITS_ID = 2;
+    public static final int MENU_EXIT_ID = 3;
+    
+    
+    public static final int DANCE_REQUEST = 0;
+    
+    
+    public static final int EXIT_RESULT_CODE = 0;
+	
+	
+	
+	private void init()
+	{
+		danceIntent = new Intent( this, Dancer.class );
+	}
+	
+	
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    	if(requestCode == DANCE_REQUEST) {
+    		if ( resultCode == EXIT_RESULT_CODE ) {
+    			
+    			finish();
+    		}
+    	}
+    }
+    
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-    	//final Intent paintIntent = new Intent( this, Paint.class );
-        final Intent paintIntent = new Intent( this, FingerPaint.class );
-        final Intent danceIntent = new Intent( this, Dancer.class );
-        final Intent creditIntent = new Intent( this, Credits.class );
-        
-        LinearLayout layout = new LinearLayout(this);
-        final Button paintButton = new Button(this);
-        final Button danceButton = new Button(this);
-        final Button creditButton = new Button(this);
-        paintButton.setText( "Paint!" );
-        paintButton.setOnClickListener(new OnClickListener()
-        {
-        	public void onClick(View view)
-        	{
-        		try
-        		{
-        			startActivity( paintIntent );
-        		}
-        		catch( ActivityNotFoundException anfe )
-        		{
-        			System.out.println( anfe.getMessage() );
-        		}
-        	}
-        });
-        
-        danceButton.setText( "Dance mofo!" );
-        danceButton.setOnClickListener(new OnClickListener()
-        {
-        	public void onClick(View view)
-        	{
-        		try
-        		{
-        			startActivity( danceIntent );
-        		}
-        		catch( ActivityNotFoundException anfe )
-        		{
-        			System.out.println( anfe.getMessage() );
-        		}
-        	}
-        });
 
-        creditButton.setText( "Credits" );
-        creditButton.setOnClickListener(new OnClickListener()
-        {
-        	public void onClick(View view)
-        	{
-        		try
-        		{
-        			startActivity( creditIntent );
-        		}
-        		catch( ActivityNotFoundException anfe )
-        		{
-        			System.out.println( anfe.getMessage() );
-        		}
-        	}
-        });
+        init();
         
-        
-
-        
-        
-        layout.addView( paintButton );
-        layout.addView( danceButton );
-        layout.addView( creditButton );
-        
-        setContentView(layout);
+        startActivityForResult(danceIntent, DANCE_REQUEST);
     }
 }
