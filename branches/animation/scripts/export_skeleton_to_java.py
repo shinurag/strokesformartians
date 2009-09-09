6a27484d81b9fa1d4ed58f611fe700e1073c8a6a
moushkka <moushkka@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-09 14:29:34 +0000

    updated exporter and leroy.java

--- branches/animation/scripts/export_skeleton_to_java.py
+++ branches/animation/scripts/export_skeleton_to_java.py
@@ -36,6 +36,24 @@ def printSkeleton(out, classname, numframes, content):
         """
 public class {classname}
 {{
+public class Bone
+    {{
+    	public String name;
+	/**
+	 * The coordinates in worldspace that defines the restpose for the bone. On the format restPose[index] where index is 0 for x and 1 for y. It only specify the head of the bone. 
+	 */
+        public float [] restPose;
+	/**
+	 * Transformation matrix for the bone on the format frames[framenumber * 16 + matrixindice] where matrixindice is a value from 0 to 15 specifying which element in a column major 4x4 matrix to use.
+	 */ 
+        public float [] frames;
+
+	public Bone(String name)
+	{{
+		this.name = name;
+	}}
+    }}
+
 public HashMap<String, Bone> bones;
         public int numFrames;
 
@@ -62,30 +80,6 @@ def createBones(out, bones):
         """
         pass
 
-def printBoneClass(out):
-    """
-    output the start of the bone class
-    """
-    out.write(
-        """
-    class Bone
-    {
-    	public String name;
-	/**
-	 * The coordinates in worldspace that defines the restpose for the bone. On the format restPose[index] where index is 0 for x and 1 for y. It only specify the head of the bone. 
-	 */
-        public float [] restPose;
-	/**
-	 * Transformation matrix for the bone on the format frames[framenumber * 16 + matrixindice] where matrixindice is a value from 0 to 15 specifying which element in a column major 4x4 matrix to use.
-	 */ 
-        public float [] frames;
-
-	public Bone(String name)
-	{
-		this.name = name;
-	}
-    }
-    """)
 
 def printMatrix(out, name, frame, matrix):
     """
@@ -99,8 +93,10 @@ def printMatrix(out, name, frame, matrix):
     for (x,col) in enumerate(tmatrix):
         for (y,elem) in enumerate(col):
             framestart = frame * 16
-            out.write("bone.frames[{index}] = {value};\n".format(index = framestart + x * 4 + y, value = elem))
+            out.write("bone.frames[{index}] = {value};\n".format(index = framestart + x * 4 + y, value = getJavaFloat(elem)))
 
+def getJavaFloat(value):
+    return str(value) + "f"
 
 def export(filename):
     """
@@ -112,8 +108,6 @@ def export(filename):
     armature = Object.Get("Armature")
 
     def filecontent(out):
-        printBoneClass(outfile)
-
         numFrames = 30
 
         def skeletoncontent(out):
@@ -129,9 +123,9 @@ def export(filename):
             for bone in armature.getData().bones.values():
                 out.write("bone = bones.get(\"{name}\");\n".format(name=bone.name))
                 # TODO: tail instead of head here?
-                out.write("bone.restPose[{index}] = {value};\n".format(index = 0, value = bone.head["ARMATURESPACE"][0]))
-                out.write("bone.restPose[{index}] = {value};\n".format(index = 0, value = bone.head["ARMATURESPACE"][1]))
-                out.write("bone.restPose[{index}] = {value};\n".format(index = 0, value = bone.head["ARMATURESPACE"][2]))
+                out.write("bone.restPose[{index}] = {value};\n".format(index = 0, value = getJavaFloat(bone.head["ARMATURESPACE"][0])))
+                out.write("bone.restPose[{index}] = {value};\n".format(index = 0, value = getJavaFloat(bone.head["ARMATURESPACE"][1])))
+                out.write("bone.restPose[{index}] = {value};\n".format(index = 0, value = getJavaFloat(bone.head["ARMATURESPACE"][2])))
 
             for frame in xrange(1,numFrames):
                 armature.evaluatePose(frame)
