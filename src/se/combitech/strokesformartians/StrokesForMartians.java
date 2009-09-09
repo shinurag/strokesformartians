4333055f318502df9138376ec0a27d3b9e293a20
moushkka <moushkka@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-09 14:49:02 +0000

    NO MORE POOP!

--- src/se/combitech/strokesformartians/StrokesForMartians.java
+++ src/se/combitech/strokesformartians/StrokesForMartians.java
@@ -35,7 +35,6 @@ public class StrokesForMartians extends Activity {
         {
         	public void onClick(View view)
         	{
-        		paintButton.setText( "pooo!" );
         		try
         		{
         			startActivity( paintIntent );
