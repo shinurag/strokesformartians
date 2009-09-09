d2ecb0354c4b1407ed33b2f61b015ddf59423d4c
Faner.Yedermann <Faner.Yedermann@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-09 15:19:14 +0000

    Animator!

--- src/se/combitech/strokesformartians/dancing/MartianAnimator.java
+++ src/se/combitech/strokesformartians/dancing/MartianAnimator.java
@@ -1,6 +1,18 @@
 package se.combitech.strokesformartians.dancing;
 
+import se.combitech.strokesformartians.Leroy2;
+
 public class MartianAnimator 
 {
-
+	private Leroy2 leroy;
+	
+	public void something()
+	{
+		
+	}
+	
+	public void getFrame( float frame )
+	{
+		
+	}
 }
