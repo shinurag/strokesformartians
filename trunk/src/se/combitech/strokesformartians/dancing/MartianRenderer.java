08340c53688d51d9d71165073fd1becbf570b0b9
Faner.Yedermann <Faner.Yedermann@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-14 15:39:00 +0000

    It compiles, once again!

--- trunk/src/se/combitech/strokesformartians/dancing/MartianRenderer.java
+++ trunk/src/se/combitech/strokesformartians/dancing/MartianRenderer.java
@@ -1,10 +1,18 @@
 package se.combitech.strokesformartians.dancing;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import se.combitech.strokesformartians.R;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.opengl.GLSurfaceView;
 
 /**
@@ -15,12 +23,19 @@ class MartianRenderer implements GLSurfaceView.Renderer {
     private boolean mTranslucentBackground;
     private MartianModel m_martian;
     private float mAngle;
-	
+    private MartianAnimator m_animator;
+    private int[] m_textureIds;
+    private Context m_context;
+	private float[] skeletonVertexBuffer;
+	private byte[] skeletonIndexBuffer; 
+    
 	public MartianRenderer( Context context, boolean useTranslucentBackground, boolean debugFlag ) {
         mTranslucentBackground = useTranslucentBackground;
         m_martian = new MartianModel( context, debugFlag );
+		m_animator = new MartianAnimator( );
+		m_textureIds = new int[1];
+		
     }
-
 	
     public void onDrawFrame(GL10 gl) {
         /*
@@ -39,7 +54,7 @@ class MartianRenderer implements GLSurfaceView.Renderer {
         gl.glMatrixMode( GL10.GL_MODELVIEW );
         gl.glLoadIdentity();
         gl.glTranslatef( 0, 0, -10.0f );
-//        gl.glRotatef( mAngle, 0, 1, 0 );
+        gl.glRotatef( -90, 1, 0, 0 );
 //        gl.glRotatef(mAngle*0.25f,  1, 0, 0);
 
 //        gl.glEnableClientState( GL10.GL_VERTEX_ARRAY );
@@ -68,6 +83,76 @@ class MartianRenderer implements GLSurfaceView.Renderer {
 
     }
 
+	private void initTextures( GL10 gl )
+	{
+    		Bitmap bitmap = BitmapFactory.decodeResource( m_context.getResources(), R.drawable.flowers );
+    				
+    		gl.glGenTextures( 	1, 
+    							m_textureIds,
+								0 );
+    		gl.glBindTexture( 	GL10.GL_TEXTURE_2D, 
+    							m_textureIds[0] );
+    		
+    		android.opengl.GLUtils.texImage2D( 	GL10.GL_TEXTURE_2D,
+							    				0,
+							    				bitmap,
+							    				0 );
+	}
+    
+    private void renderMartianAnimator( GL10 gl )
+    {
+    	gl.glColor4f( 0, 1, 1, 1 );
+        gl.glEnable( GL10.GL_TEXTURE_2D );
+        
+    	if( m_textureIds[0] == 0 )
+    	{
+    		initTextures( gl );
+    	}
+        
+    	gl.glBindTexture( 	GL10.GL_TEXTURE_2D, 
+							m_textureIds[0] );
+		
+		gl.glTexEnvf( 	GL10.GL_TEXTURE_ENV, 
+						GL10.GL_TEXTURE_ENV_MODE, 
+						GL10.GL_DECAL );
+
+		m_animator.getSkeletonFrame( 0, skeletonVertexBuffer, skeletonIndexBuffer );
+		
+		gl.glDisable( GL10.GL_TEXTURE_2D );
+		gl.glColor4f( 0, 0, 0, 1 );
+		try {
+			ByteBuffer vertexByteBuffer;
+			vertexByteBuffer = ByteBuffer.allocateDirect( skeletonVertexBuffer.length * 4 );
+			vertexByteBuffer.order( ByteOrder.nativeOrder() );
+			FloatBuffer vertexBuffer = vertexByteBuffer.asFloatBuffer();
+			vertexBuffer.put( skeletonVertexBuffer );
+			vertexBuffer.position( 0 );
+			
+			ByteBuffer mIndexBuffer = ByteBuffer.allocateDirect( skeletonIndexBuffer.length );
+			mIndexBuffer.put( skeletonIndexBuffer );
+			mIndexBuffer.position( 0 );
+
+	        gl.glEnableClientState( GL10.GL_VERTEX_ARRAY );
+			
+			gl.glVertexPointer( 	3,
+									GL10.GL_FLOAT, 
+									0,
+									vertexBuffer );
+			
+			gl.glLineWidth( 2 );
+			
+			gl.glDrawElements( 	GL10.GL_LINES, 
+				  				30, 
+				  				GL10.GL_UNSIGNED_BYTE, 
+				  				mIndexBuffer );
+			
+	        gl.glDisableClientState( GL10.GL_VERTEX_ARRAY );
+			
+		} catch ( Exception e ) {
+			e.printStackTrace();
+		}
+    }    	
+    
     public int[] getConfigSpec() {
         if (mTranslucentBackground) {
                 // We want a depth buffer and an alpha buffer
