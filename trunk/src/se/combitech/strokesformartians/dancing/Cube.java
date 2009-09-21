e28192017f1b8aaedc2cd99ee5ddae827d23533e
Faner.Yedermann <Faner.Yedermann@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-21 17:57:12 +0000

    RETARDED!

--- trunk/src/se/combitech/strokesformartians/dancing/Cube.java
+++ trunk/src/se/combitech/strokesformartians/dancing/Cube.java
@@ -14,7 +14,7 @@ class Cube
 {
     public Cube()
     {
-        int one = 0x1;
+        int one = 1;
         float vertices[] = {
                 -one, -one, -one,
                 one, -one, -one,
@@ -26,6 +26,24 @@ class Cube
                 -one,  one,  one,
         };
 
+        float texCoords[] = {
+        		0.25f, 0.00f,  0.50f, 0.33f,  0.25f,0.33f,    0.25f, 0.00f,  0.50f, 0.33f,  0.50f, 0.00f,
+        		0f,0f, 0f,0f, 0f,0f,  0f,0f, 0f,0f, 0f,0f,
+
+        		0f,0f, 0f,0f, 0f,0f,  0f,0f, 0f,0f, 0f,0f,
+        		0f,0f, 0f,0f, 0f,0f,  0f,0f, 0f,0f, 0f,0f,
+
+        		0f,0f, 0f,0f, 0f,0f,  0f,0f, 0f,0f, 0f,0f,
+        		0f,0f, 0f,0f, 0f,0f,  0f,0f, 0f,0f, 0f,0f
+
+//        		0.75f, 0.33f,  0.50f, 0.33f,  0.50f,0.66f,    0.75f, 0.33f,  0.50f, 0.66f,  0.75f, 0.66f,
+//        		0.50f, 1.00f,  0.50f, 0.66f,  0.25f,0.66f,    0.50f, 1.00f,  0.25f, 0.66f,  0.25f, 1.00f,
+//        		
+//        		0.00f, 0.66f,  0.25f, 0.66f,  0.25f,0.33f,    0.00f, 0.66f,  0.25f, 0.33f,  0.00f, 0.33f,
+//        		0.25f, 0.33f,  0.25f, 0.66f,  0.50f,0.66f,    0.25f, 0.33f,  0.50f, 0.66f,  0.50f, 0.33f,
+//        		1.00f, 0.66f,  1.00f, 0.33f,  0.75f,0.33f,    1.00f, 0.66f,  0.75f, 0.33f,  0.75f, 0.66f
+        };
+        
         int colors[] = {
                 0,    0,    0,  one,
                 one,    0,    0,  one,
@@ -59,6 +77,12 @@ class Cube
         mVertexBuffer = vbb.asFloatBuffer();
         mVertexBuffer.put(vertices);
         mVertexBuffer.position(0);
+        
+        ByteBuffer tbb = ByteBuffer.allocateDirect(texCoords.length*4);
+        tbb.order(ByteOrder.nativeOrder());
+        mTexCoordBuffer = tbb.asFloatBuffer();
+        mTexCoordBuffer.put(texCoords);
+        mTexCoordBuffer.position(0);
 
         ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length*4);
         cbb.order(ByteOrder.nativeOrder());
@@ -71,15 +95,25 @@ class Cube
         mIndexBuffer.position(0);
     }
 
-    public void draw(GL10 gl)
+    public void draw( GL10 gl )
     {
-        gl.glFrontFace(gl.GL_CW);
-        gl.glVertexPointer(3, gl.GL_FLOAT, 0, mVertexBuffer);
-        gl.glColorPointer(4, gl.GL_FIXED, 0, mColorBuffer);
-        gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_BYTE, mIndexBuffer);
+        gl.glFrontFace( gl.GL_CW );
+        gl.glVertexPointer( 3, gl.GL_FLOAT, 0, mVertexBuffer );
+        gl.glColorPointer( 4, gl.GL_FIXED, 0, mColorBuffer );
+        gl.glTexCoordPointer( 36, gl.GL_FLOAT, 0, mTexCoordBuffer );
+        
+		gl.glEnableClientState( GL10.GL_VERTEX_ARRAY );
+		gl.glEnableClientState( GL10.GL_TEXTURE_COORD_ARRAY );
+        
+        gl.glDrawElements( gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_BYTE, mIndexBuffer );
+        
+		gl.glDisableClientState( GL10.GL_VERTEX_ARRAY );
+		gl.glDisableClientState( GL10.GL_TEXTURE_COORD_ARRAY );
+        
     }
 
     private FloatBuffer   mVertexBuffer;
+    private FloatBuffer   mTexCoordBuffer;
     private IntBuffer   mColorBuffer;
     private ByteBuffer  mIndexBuffer;
 }
