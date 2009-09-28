0df585c6e2f311a08f7d65d9543dc520e5317f7c
moushkka <moushkka@4897e53a-4ec2-11de-9241-1f6e7bca54ae>
2009-09-28 16:19:50 +0000

    BrushSize now works!

--- trunk/src/se/combitech/strokesformartians/drawing/BrushSizeDialog.java
+++ trunk/src/se/combitech/strokesformartians/drawing/BrushSizeDialog.java
@@ -2,63 +2,70 @@ package se.combitech.strokesformartians.drawing;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.graphics.*;
-import android.graphics.drawable.GradientDrawable.Orientation;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.LinearLayout.LayoutParams;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class BrushSizeDialog extends Dialog{
-	final float MAX_SIZE = 132.0f;
-	final float MIN_SIZE = 6.0f;	
+	final float MAX_SIZE = 200.0f;
+	final float MIN_SIZE = 12.0f;	
 	float currentSize = 20.0f;
-	private OnBrushSizeChangeListener mListener;
+	private android.view.View.OnClickListener mClickListener;
+	private OnSeekBarChangeListener mListener;
 	private View mySizePickerView;
 	
-	private float mInitialSize = 12.0f;
+	private float mInitialSize = 20.0f;
 	
-	private SeekBar seekBar = new SeekBar(getContext());
+	private SeekBar seekBar;
 	
     public interface OnBrushSizeChangeListener {
         void onBrushSizeChange(float size);
     }
     
-    
-//    
-//	private final int r;
-//    private final Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-//    
-	private static class SizePickerView extends View {
+	public static class SizePickerView extends View {
     	
-    	private OnBrushSizeChangeListener mListener;
+    	private OnSeekBarChangeListener mListener;
     	private Paint mPaint;
-    	private float mSize = 20;
+    	private float mSize = 20.0f;
     	private BrushSizeDialog mCallback;
     	
-    	SizePickerView(Context c, OnBrushSizeChangeListener l, float s) {
-//    	SizePickerView(Context c) {	
+    	SizePickerView(Context c, OnSeekBarChangeListener listener, float s) {
     		super(c);
-    		mListener = l;
+    		mListener = listener;
 
-        	mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        	mPaint.setStyle(Paint.Style.STROKE);
-            mPaint.setStrokeWidth(32);
+
     	}    	
     	
+    	public void updateSize(float size) {	
+    		mSize = size;
+    	}
+    	
+    	public float getSize() {
+    		return mSize;
+    	}
         
     	@Override
     	protected void onDraw(Canvas canvas) {
     	    super.onDraw(canvas);
-    	    canvas.setViewport(50, 70);
-    	    canvas.drawColor(Color.RED);
-    	    canvas.drawCircle(0, 0, mSize, mPaint);
+//    	    canvas.setViewport(50, 70);
+//    	    canvas.drawColor(Color.RED);
+    	    
+    	    // Draw the circle
+        	mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        	mPaint.setStyle(Paint.Style.FILL);
+//            mPaint.setStrokeWidth(32);
+            
+    	    int centerX = canvas.getWidth() / 2;
+    	    int centerY = canvas.getHeight() / 2;
+    	    canvas.drawCircle(centerX, centerY, mSize, mPaint);
     	    
     	}
     	
@@ -70,8 +77,8 @@ public class BrushSizeDialog extends Dialog{
                     
                 	break;
                 case MotionEvent.ACTION_MOVE:                    
-                	mSize = mCallback.getSeekBarValue();
-                    invalidate();
+//                	mSize = mCallback.getSeekBarValue();
+//                    invalidate();
                     
                     break;
                 case MotionEvent.ACTION_UP:
@@ -87,10 +94,12 @@ public class BrushSizeDialog extends Dialog{
     	}
     }
 //	
-	public BrushSizeDialog(Context context, OnBrushSizeChangeListener bsListener, float size) {
+	public BrushSizeDialog(Context context, android.view.View.OnClickListener clickListener, float size) {
 		super(context);
-		mListener = bsListener;
+		mClickListener = clickListener;
 		mInitialSize = size;
+		
+		seekBar = new SeekBar(context);
 	}
 
 	public float getSeekBarValue() {
@@ -99,14 +108,29 @@ public class BrushSizeDialog extends Dialog{
 	
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-                      
-        OnBrushSizeChangeListener l = new OnBrushSizeChangeListener() {
-			public void onBrushSizeChange(float size) {
-				mListener.onBrushSizeChange(size);				
-				dismiss();
+        
+        OnSeekBarChangeListener mListener = new OnSeekBarChangeListener() {
+
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				currentSize = getSeekBarValue();
+				((SizePickerView)mySizePickerView).updateSize(currentSize);
+            	mySizePickerView.invalidate();
 			}
+
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+        	
         };
-       
+		seekBar.setOnSeekBarChangeListener(mListener);
+		
         mySizePickerView = new SizePickerView(getContext(), mListener, currentSize);
         ((SizePickerView)mySizePickerView).setCallback(this);
         
@@ -133,13 +157,13 @@ public class BrushSizeDialog extends Dialog{
 		
 		Button helloButton = new Button(getContext());
 		helloButton.setText("Ok");
+		helloButton.setOnClickListener(mClickListener);
 		myLayout.addView(helloButton);		
-		  
-		myLayout.addView(mySizePickerView);
-		setContentView(mySizePickerView);
 		
-		setTitle("Pick a brush size, fool!");        
+		myLayout.addView(mySizePickerView);
+		setContentView(myLayout);
+		
+		setTitle("Pick a brush size");        
     }
-	
-	
+
 }
